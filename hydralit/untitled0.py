3a4a43068617b6ae42cb66dfac49485e16eb943d
ARSHDEEP SINGH <2arshahuja@gmail.com>
2022-04-10 12:12:16 -0700

    Dashboard V1

---
+++ hydralit/untitled0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 15:01:30 2022
+
+@author: arshdeepsingh
+"""
+
+body {
+    background-color: #eee;
+}
+
+.fullScreenFrame > div {
+    display: flex;
+    justify-content: center;
+}
